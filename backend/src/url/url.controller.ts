8343883383b0cb33e48e2b80a58c4f625c360c8c
M. Britto <miqueias.silva@alghorit.com.br>
2026-09-04 10:56:45 -0300

    refactor UrlController to use @Body for URL input and handle missing URL with BadRequestException

--- backend/src/url/url.controller.ts
+++ backend/src/url/url.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Req, Res } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post, Req, Res } from '@nestjs/common';
 import { UrlService } from './url.service';
 import type { Request, Response } from 'express';
 
@@ -7,30 +7,12 @@ export class UrlController {
   constructor(private readonly urlService: UrlService) {}
 
   @Post()
-  async create(
-    @Req() req: Request,
-    @Res() res: Response
+  async create(@Body() body: { url:string }
   ) {
-    try {
-      const { url } = req.body
-
-      if(!url) {
-        return res.status(400).json({
-          message: "Missing required fields"
-        })
-      }
-
-      const newUrl = await this.urlService.createShortUrl(url);
-
-      return res.status(200).json(newUrl);
-
-    } catch (error) {
-        if (error instanceof Error) {
-          return res.status(404).json({
-            message: error.message
-          })
-        }
-
+    if(!body.url) {
+      throw new BadRequestException('URL is required');
     }
+
+    return this.urlService.createShortUrl(body.url);
   }
 }
